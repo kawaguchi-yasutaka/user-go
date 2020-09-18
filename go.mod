3a3dbe2728c6d72b5fd8a5025d7c161e2644d15c
kawaguchi <kawaguchi.yasutaka@gmail.com>
2020-09-18 20:13:18 +0900

    feat(): godotenvを追加

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module user-go
 go 1.13
 
 require (
+	github.com/joho/godotenv v1.3.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
 	golang.org/x/sys v0.0.0-20200917073148-efd3b9a0ff20 // indirect
--- initializer/db.go
+++ initializer/db.go
@@ -1,13 +1,16 @@
 package initializer
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 	inframysql "user-go/infra/mysql"
 )
 
 func InitDbConnection() *gorm.DB {
-	dsn := "master:gagagigu123@tcp(127.0.0.1:3306)/usergo?charset=utf8&parseTime=True"
+	dsn := getDbDsn()
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
@@ -18,4 +21,19 @@ func InitDbConnection() *gorm.DB {
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(&inframysql.User{})
 	db.AutoMigrate(&inframysql.UserAuthentication{})
+}
+
+func getDbDsn() string {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
+		os.Getenv("DB_USER_NAME"),
+		os.Getenv("DB_USER_PASSWORD"),
+		os.Getenv("DB_ADDRESS"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_TABLE"),
+	)
 }
